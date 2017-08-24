6b46e4d7b2319c29da960f383d6db6b24411c03e
Samiksha Chandak <schandak@tavisca.com>
2017-08-24 15:09:50 +0530

    automobile showroom management model

---
+++ AutomobileShowroomManagement/AutomobileShowroomManagement/Vehicle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomobileShowroomManagement
+{
+    public class Vehicle
+    {
+        public string Name { get; private set; }
+        public string Color { get; private set; }
+        public VehicleCondition Condition { get; set; }
+        public bool Status { get; set; }
+        public int Milege { get; set; }
+        public string VehicleNumber { get; set; }
+
+        public Vehicle(string name, string color, VehicleCondition condition, bool status,int milege,string vehicleNumber)
+        {
+            Name = name;
+            Color = color;
+            Condition = condition;
+            Status = status;
+            Milege = milege;
+            VehicleNumber = vehicleNumber;
+        }
+        
+        
+    }
+}
